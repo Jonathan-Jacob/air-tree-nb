ae0926205088ca687a3515318927ee6fa45fad34
Fabian Schroeder <f.schroed88@gmx.de>
2021-02-23 12:14:25 +0100

    pundit added in application controller application policy added added booking pundit added treehouse pundit added user pundit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_160732) do
+ActiveRecord::Schema.define(version: 2021_02_22_164623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "total_price"
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "treehouse_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["treehouse_id"], name: "index_bookings_on_treehouse_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "treehouses", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price_per_day"
+    t.integer "cancel_days"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_treehouses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +50,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_160732) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "treehouses"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "treehouses", "users"
 end
