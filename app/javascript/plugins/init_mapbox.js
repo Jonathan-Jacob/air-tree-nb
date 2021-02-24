4cffb0aab3a15d1724e0dedcdf02d92f0fcac374
Jonathan Jacob <jjjscf@gmail.com>
2021-02-24 14:44:10 +0100

    map style change

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,7 +13,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/jonathanj/ckljhff0p0ele17pezf8tn3nr'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
