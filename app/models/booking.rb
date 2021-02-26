80d2ffa0676cde44abc80c5d74958fa2abadae44
Julius von Rohr <juliusvrohr@gmail.com>
2021-02-26 13:14:47 +0100

    changed avatar and bookingsmodel

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,10 +6,17 @@ class Booking < ApplicationRecord
   validates :guests, presence: true
 
   validate :end_date_after_start_date?
+  validate :guests_match_capacity?
 
   def end_date_after_start_date?
     if end_date < start_date
       errors.add :end_date, "must be after start date"
+    end
+  end
+
+  def guests_match_capacity?
+    if guests > treehouse.capacity
+      errors.add :guests, "max. #{treehouse.capacity}"
     end
   end
 end
